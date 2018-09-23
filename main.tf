c75b99808721b7c2035df501b8ecfd520bdd4f4f
Jenkins Automation Server <jenkins@instance-1.us-central1-a.c.kube-test-216119.internal>
2018-09-23 00:35:11 +0000

    44 commit

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ provider "aws" {
 }
 
 resource "aws_security_group" "lnx_web" {
-  name        = NAME
-  description = DESCRIPTION
+  name        = "hi"
+  description = "bye"
   
 	tags {
         Name = "lnx_web"
